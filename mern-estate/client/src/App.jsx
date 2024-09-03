26125b6c01252514d64baa6c98e3d134c571c92a
pasyet <fapasyetnabila@gmail.com>
2024-09-03 17:34:26 +0700

    create header component

--- mern-estate/client/src/App.jsx
+++ mern-estate/client/src/App.jsx
@@ -3,11 +3,13 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />

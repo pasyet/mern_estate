247d9b6be84ea913d5704f335d6c8810decde099
pasyet <fapasyetnabila@gmail.com>
2024-09-03 15:51:09 +0700

    create pages and routes

--- mern-estate/client/src/App.jsx
+++ mern-estate/client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import  { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+
 
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }

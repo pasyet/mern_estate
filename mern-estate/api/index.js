d5e7c032ce24eefc56c02cdde945b5c121a54e0b
pasyet <fapasyetnabila@gmail.com>
2024-09-13 00:42:35 +0700

    create sign up api route

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,9 +17,12 @@ mongoose
 
 
 const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
 });
 
 app.use('/api/user', userRouter); 
+app.use('/api/auth', authRouter);

c52a4be61063ffbe920802b57e2c03f8dc700fc4
pasyet <fapasyetnabila@gmail.com>
2024-09-13 01:13:11 +0700

    create sign up page ui

--- mern-estate/client/src/App.jsx
+++ mern-estate/client/src/App.jsx
@@ -2,6 +2,7 @@ import  { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>

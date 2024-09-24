a3aefaf5f2cf616f12c388fc7a3ff8235a47e07f
pasyet <fapasyetnabila@gmail.com>
2024-09-24 23:56:21 +0700

    complate create listing page UI

--- mern-estate/client/src/App.jsx
+++ mern-estate/client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import CreateListing from './pages/CreateListing';
 import PrivateRoute from './components/PrivateRoute';
 
 
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
           </Route>
       </Routes>
     </BrowserRouter>
--- mern-estate/client/src/pages/Profile.jsx
+++ mern-estate/client/src/pages/Profile.jsx
@@ -13,6 +13,7 @@ import {
     signOutUserSuccess
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 
 export default function Profile() {
@@ -180,6 +181,9 @@ const handleSignOut = async () => {
                     className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
                     {loading ? 'Loading...' : 'Update'}
                 </button>
+                <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'to = {'/create-listing'}>
+                    Create Listing
+                </Link>
             </form>
             <div className='flex justify-between mt-5'>
                 <span onClick= {handleDeleteUser} className='text-red-700 cursor-pointer'>
